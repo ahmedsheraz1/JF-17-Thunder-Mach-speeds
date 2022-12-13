c97aaba1f9bb463db4e2b8a420978c34d9993bbf
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 01:35:29 +0500

    Create JF-17 Thunder.py

---
+++ JF-17 Thunder.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'JF-17 Thunder Jet').hexdigest()
+'316a923163c0a09758aa6354e5774c695da6abd20015f4f279f1beac39361242cd86d55f98c4b070a05d0847967785f04fd9c9bfffcaa5aa5037debb9f5d223f'
